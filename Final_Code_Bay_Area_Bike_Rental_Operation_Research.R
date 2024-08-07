58303a88b9b9d3b7c81cd9d1a2317688b65064e4
Taran Bhartt <Tarzor101@gmail.com>
2024-08-07 12:41:24 -0400

    Transfered over the data cleaning code from the EDA files

--- Final_Code_Bay_Area_Bike_Rental_Operation_Research.R
+++ Final_Code_Bay_Area_Bike_Rental_Operation_Research.R
@@ -86,11 +86,119 @@ CleanedWeather$city <- factor(CleanedWeather$city)
 
 # d) Analyze categorical variables: do the categories make sense? Missingness?
 
+# Summarize the categorical variables to get a sense of the number of NAs and the distribution of values, as well as any values that represent the same thing but are coded differently
+summary(CleanedStation$id)
+summary(CleanedStation$name)
+summary(CleanedStation$city)
+summary(CleanedStation$installation_date)
+# no missingness in the station categorical variables
+
+summary(CleanedWeather$events)
+# high degree of missingness: 1473 Not Recorded - may indicate no weather events (sunny days are not coded for)
+# Ways to impute missingness:
+# - Fog: visibility less than 1 km
+# - Mist: visibility between 1 km (0.62 mi) and 2 km (1.2 mi)
+# - Haze from 2 km (1.2 mi) to 5 km (3.1 mi)
+# - Temperatures at 14 F (-10°C) is too cold for the air to contain super-cooled 
+#   water droplets so for forms small tiny ice crystals.
+# - Rain: any amount of precipitation, even trace
+# - Thunderstorm: hot temperatures, cloud cover, wind-speed, but highly variable
+
+CleanedWeather <- mutate(CleanedWeather, ImputedEvents = case_when(
+  precipitation_inches=0 & min_visibility_miles<0.62 ~ "Fog",
+  precipitation_inches=0 & min_visibility_miles<1.2 & min_visibility_miles>0.62 ~ "Mist",
+  precipitation_inches=0 & min_visibility_miles<3.1 & min_visibility_miles>1.2 ~ "Haze",
+  precipitation_inches>0 & min_visibility_miles<0.62 ~ "Fog-Rain",
+  precipitation_inches>0 & min_visibility_miles<1.2 & min_visibility_miles>0.62 ~ "Mist-Rain",
+  precipitation_inches>0 & min_visibility_miles<3.1 & min_visibility_miles>1.2 ~ "Haze-Rain",
+  precipitation_inches>0 & min_visibility_miles>3.1 ~ "Rain",
+  .default = CleanedWeather$events
+))
+CleanedWeather$ImputedEvents <- factor(CleanedWeather$ImputedEvents)
+summary(CleanedWeather$ImputedEvents) #the imputed events account for some missingness
+#very limited efficacy for imputation, as too much information is missing. Still, 1423 events are missing - may indicate no weather events (sunny days are not coded for)
+plot(CleanedWeather$ImputedEvents)
+
+summary(CleanedWeather$city)
+# no missing cities, equal distribution
+plot(CleanedWeather$city)
+
+summary(CleanedWeather$date)
+# no times, only dates
+
 # e) Analyze numerical variables: outliers? What is the distribution of data? What are the standard deviations?
+summary(CleanedStation$lat)
+summary(CleanedStation$long)
+summary(CleanedStation$dock_count)
+ggplot(CleanedStation, mapping = aes(x=long, y=lat, col = dock_count)) + geom_point() + scale_colour_gradient()
+# This maps out the stations by latitude and longitude, colour-coded by their number of docks
+# It doesn't make sense to remove outliers given these are descriptive counting numbers: the latitude, longitude, number of docks, and are unlikely to reflect an error in measurement. Even if there is a higher-than-average number of docks, there is no clear reason as to why it should be excluded, or how it will negatively affect the analysis 
+
+summary(CleanedWeather$max_temperature_f) #examine the range of maximum temperatures
+sd(CleanedWeather$max_temperature_f)
+plot(CleanedWeather$date,CleanedWeather$max_temperature_f, xlab="Date", ylab="Maximum Temperature (F)")
+summary(CleanedWeather$mean_temperature_f)
+sd(CleanedWeather$mean_temperature_f)
+plot(CleanedWeather$date,CleanedWeather$mean_temperature_f, xlab="Date", ylab="Mean Temperature (F)")
+summary(CleanedWeather$min_temperature_f)
+sd(CleanedWeather$min_temperature_f)
+plot(CleanedWeather$date,CleanedWeather$min_temperature_f, xlab="Date", ylab="Minimum Temperature (F)")
+summary(CleanedWeather$max_visibility_miles)
+sd(CleanedWeather$max_visibility_miles)
+plot(CleanedWeather$date,CleanedWeather$max_visibility_miles, xlab="Date", ylab="Maximum Visibility (miles)")
+summary(CleanedWeather$mean_visibility_miles)
+sd(CleanedWeather$mean_visibility_miles)
+plot(CleanedWeather$date,CleanedWeather$mean_visibility_miles, xlab="Date", ylab="Mean Visibility (miles)")
+summary(CleanedWeather$min_visibility_miles)
+sd(CleanedWeather$min_visibility_miles)
+plot(CleanedWeather$date,CleanedWeather$min_visibility_miles, , xlab="Date", ylab="Minimum Visibility (miles)")
+summary(CleanedWeather$max_wind_Speed_mph)
+sd(CleanedWeather$max_wind_Speed_mph)
+plot(CleanedWeather$date,CleanedWeather$max_wind_Speed_mph, xlab="Date", ylab="Maximum Wind Speed (mph)")
+summary(CleanedWeather$mean_wind_speed_mph)
+sd(CleanedWeather$mean_wind_speed_mph)
+plot(CleanedWeather$date,CleanedWeather$mean_wind_speed_mph, xlab="Date", ylab="Mean Wind Speed (mph)")
+summary(CleanedWeather$max_gust_speed_mph)
+sd(CleanedWeather$max_gust_speed_mph)
+plot(CleanedWeather$date,CleanedWeather$max_gust_speed_mph, xlab="Date", ylab="Maximum Gust Speed (mph)")
+summary(CleanedWeather$precipitation_inches)
+sd(CleanedWeather$precipitation_inches)
+plot(CleanedWeather$date,CleanedWeather$precipitation_inches, xlab="Date", ylab="Precipitation (inches)")
+summary(CleanedWeather$cloud_cover)
+sd(CleanedWeather$cloud_cover)
+plot(CleanedWeather$date,CleanedWeather$cloud_cover, xlab="Date", ylab="Cloud Cover")
 
 # f) Analyze categorical and numerical variables at the same time: get a sense of all the variables to be analyzed
 
+basic_eda <- function(data)
+{
+  glimpse(data)
+  print(status(data))
+  freq(data) 
+  print(profiling_num(data))
+  plot_num(data)
+  describe(data)
+}
+
+basic_eda(CleanedStation)
+basic_eda(CleanedTrip)
+basic_eda(CleanedWeather)
+
 # 3. Record trip IDs for outliers, and then remove them. What are the rules for outliers? Outside of 95% of the data? Or a different metric?
+CleanedTrip <- mutate(CleanedTrip, TripDuration = (CleanedTrip$end_date-CleanedTrip$start_date))
+# figure out which trips lasted longer than 1 month
+TripDurationOutlierIndices <- which(CleanedTrip$TripDuration>as.difftime(43200,units="mins"))
+TripDurationOutliers <- CleanedTrip[TripDurationOutlierIndices,]
+# 1440 minutes constitutes a full day of bike renting - Bay Area bike rental lengths usually go up to full-day or 24 hours (though there are some that can be rented on a weekly, or even monthly basis) - https://www.lyft.com/bikes/bay-wheels/sf-bike-rental
+CleanedTrip <- CleanedTrip[-TripDurationOutlierIndices,]
+Reduced = as.numeric(readline(prompt = "Please enter a 1 if you want to analyze 97.5% of the possible data, or enter 0 to analyze all possible data. Both exclude outliers"))
+if (Reduced == 1){
+  # determine which trip durations fall outside of the upper bounds of the 95% confidence interval
+  TripDurationExtremeIndices <- which(CleanedTrip$TripDuration>=quantile(CleanedTrip$TripDuration,probs=0.975)) #this will include the outliers as well
+  # Place the outliers in a new data frame
+  TripDurationExtremeValues <- CleanedTrip[setdiff(TripDurationExtremeIndices,TripDurationOutlierIndices),]
+  CleanedTrip <- CleanedTrip[-TripDurationExtremeIndices,] #remove the outlier indices from the main cleaned dataframe
+}
 
 # 4. Record and remove "cancelled trips" (i.e., any trip <3 minutes)
 
