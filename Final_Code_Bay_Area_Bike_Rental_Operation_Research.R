9126954ab43a6036ad8dca588e1f52b460d7e2e5
Taran Bhartt <Tarzor101@gmail.com>
2024-08-07 13:26:01 -0400

    Add correlation models to the code using code from previous scripts

--- Final_Code_Bay_Area_Bike_Rental_Operation_Research.R
+++ Final_Code_Bay_Area_Bike_Rental_Operation_Research.R
@@ -377,5 +377,87 @@ plot(x = OverallBikeUsage[,2], y= OverallBikeUsage[,1]*100, xlab="Month", ylab="
 
 # 9. Create a new dataset combining trip data with weather data. Weather data is available for each city and date. The cor function from the Corrplot package will be useful for creating a correlation matrix. Deal with the fact that it is difficult to correlate categorical and non-categorical variables. Some trips start and end in different cities, will need to explain how to correlate weather data with trip data if the trip is crossing cities - may have implications for how the two datasets are joined
 
+# Need to make the dates applicable in teh trip dataset so that they work with the weather dataset
+CleanedTrip$start_date <- floor_date(CleanedTrip$start_date, "day") #make the start dates applicable for comparison with the dates shown in the weather data
+CityZipData <- left_join(x=CleanedStation, y=CleanedTrip, by=c("id"="start_station_id"))
+CityZipData <- CityZipData[,c(2,3,4,6,10,12,13,14,16,17,18)]
+CityZipData <- left_join(x=CityZipData, y=CleanedWeather, by=c("city"="city", "start_date"="date"))
+
+# focus on the start date as the weather during the initiation of the trip would impact whether someone chooses to use a bike or use a different mode of transportation
+
+CorrelationWeatherAndTripData1 <- CityZipData[,c(4,10:22,24)]
+test1 <- model.matrix(~0+., data=CorrelationWeatherAndTripData1)
+cor(x=test1[,c(7:27)],y=test1[,c(1:6)],use="pairwise.complete.obs") %>%
+  ggcorrplot(show.diag=NULL, type="full", lab=TRUE, lab_size=2,
+             title = "Weather and Trip Variable Correlation",
+             colors = c("blue", "white", "red"))
+
+
+CorrelationWeatherAndTripData <- CityZipData[,c(1,10:22)]
+test <- model.matrix(~0+., data=CorrelationWeatherAndTripData)
+cor(x=test[,c(1:10)],y=test[,c(72:85)],use="pairwise.complete.obs") %>%
+  corrplot(show.diag=NULL, method = "color", type="upper", mar=c(0,0,2,0),
+           tl.col = "black",  cl.ratio = 0.5, cl.length=5, tl.offset=0,title = "Weather and Trip Variable Correlation")
+
+cor(x=test[,c(11:20)],y=test[,c(72:85)],use="pairwise.complete.obs") %>%
+  corrplot(show.diag=NULL, method = "color", type="upper", mar=c(0,0,2,0),
+           tl.col = "black",  cl.ratio = 0.5, cl.length=5, tl.offset=0,title = "Weather and Trip Variable Correlation")
+
+cor(x=test[,c(21:30)],y=test[,c(72:85)],use="pairwise.complete.obs") %>%
+  corrplot(show.diag=NULL, method = "color", type="upper", mar=c(0,0,2,0),
+           tl.col = "black",  cl.ratio = 0.5, cl.length=5, tl.offset=0,title = "Weather and Trip Variable Correlation")
+
+cor(x=test[,c(31:40)],y=test[,c(72:85)],use="pairwise.complete.obs") %>%
+  corrplot(show.diag=NULL, method = "color", type="upper", mar=c(0,0,2,0),
+           tl.col = "black",  cl.ratio = 0.5, cl.length=5, tl.offset=0,title = "Weather and Trip Variable Correlation")
+
+cor(x=test[,c(41:50)],y=test[,c(72:85)],use="pairwise.complete.obs") %>%
+  corrplot(show.diag=NULL, method = "color", type="upper", mar=c(0,0,2,0),
+           tl.col = "black",  cl.ratio = 0.5, cl.length=5, tl.offset=0,title = "Weather and Trip Variable Correlation")
+
+cor(x=test[,c(51:60)],y=test[,c(72:85)],use="pairwise.complete.obs") %>%
+  corrplot(show.diag=NULL, method = "color", type="upper", mar=c(0,0,2,0),
+           tl.col = "black",  cl.ratio = 0.5, cl.length=5, tl.offset=0,title = "Weather and Trip Variable Correlation")
+
+cor(x=test[,c(61:65)],y=test[,c(72:85)],use="pairwise.complete.obs") %>%
+  corrplot(show.diag=NULL, method = "color", type="upper", mar=c(0,0,2,0),
+           tl.col = "black",  cl.ratio = 0.5, cl.length=5, tl.offset=0,title = "Weather and Trip Variable Correlation")
+
+cor(x=test[,c(66:71)],y=test[,c(72:85)],use="pairwise.complete.obs") %>%
+  corrplot(show.diag=NULL, method = "color", type="upper", mar=c(0,0,2,0),
+           tl.col = "black",  cl.ratio = 0.5, cl.length=5, tl.offset=0,title = "Weather and Trip Variable Correlation")
+
+
+# Linear model
+
+TripDurationLinearModel <- lm(as.numeric(TripDuration) ~ max_temperature_f+mean_temperature_f+min_temperature_f+max_visibility_miles+mean_visibility_miles+min_visibility_miles+max_wind_Speed_mph+mean_wind_speed_mph+max_gust_speed_mph+precipitation_inches+cloud_cover+events, data=CityZipData)
+summary(TripDurationLinearModel)
+
+TripDurationLinearModel2 <- lm(as.numeric(TripDuration) ~ mean_visibility_miles+precipitation_inches+cloud_cover+events, data=CityZipData)
+summary(TripDurationLinearModel2)
+
+TripDurationLinearModel3 <- lm(as.numeric(TripDuration) ~ mean_visibility_miles+precipitation_inches+events, data=CityZipData)
+summary(TripDurationLinearModel3)
+
+# It is plausible that customers and subscribers behave differently, and have different purposes for using bikes (i.e. recreation vs work)
+
+CityZipDataCustomer <- CityZipData[CityZipData$subscription_type=="Customer",]
+CorrelationWeatherAndTripData1 <- CityZipDataCustomer[,c(4,11:23,25)]
+test1 <- model.matrix(~0+., data=CorrelationWeatherAndTripData1)
+cor(x=test1[,c(7:27)],y=test1[,c(1:6)],use="pairwise.complete.obs") %>%
+  ggcorrplot(show.diag=NULL, type="full", lab=TRUE, lab_size=2,
+             title = "Weather and Trip Variable Correlation",
+             colors = c("blue", "white", "red"))
+
+CityZipDataSubscriber <- CityZipData[CityZipData$subscription_type=="Subscriber",]
+CorrelationWeatherAndTripData1 <- CityZipDataSubscriber[,c(4,11:23,25)]
+test1 <- model.matrix(~0+., data=CorrelationWeatherAndTripData1)
+cor(x=test1[,c(7:27)],y=test1[,c(1:6)],use="pairwise.complete.obs") %>%
+  ggcorrplot(show.diag=NULL, type="full", lab=TRUE, lab_size=2,
+             title = "Weather and Trip Variable Correlation",
+             colors = c("blue", "white", "red"))
+
+
+
 
 
