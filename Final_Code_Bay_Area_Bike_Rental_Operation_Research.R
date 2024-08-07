db1cd52a2b5b08296ca729366a653a75cd587216
Taran Bhartt <Tarzor101@gmail.com>
2024-08-07 13:03:06 -0400

    Carried over and commmented on trip analysis for rush hour

--- Final_Code_Bay_Area_Bike_Rental_Operation_Research.R
+++ Final_Code_Bay_Area_Bike_Rental_Operation_Research.R
@@ -202,12 +202,180 @@ if (Reduced == 1){
 
 # 4. Record and remove "cancelled trips" (i.e., any trip <3 minutes)
 
+CancelledTripIndices <- which(CleanedTrip$TripDuration<as.difftime(3,units="mins")) #find which trips lasted less than 3 minutes
+CancelledTripValues <- CleanedTrip[CancelledTripIndices,]
+CleanedTrip <- CleanedTrip[-CancelledTripIndices,]
+
+boxplot(CleanedTrip$TripDuration, #plot the final distribution of trips
+        ylab = "Duration (mins)"
+)
+title("Cleaned Bike Trip Durations")
+
 # 5. Determine weekday rush hours - What consists of rush hour? Provide a definition. Histograms by hour are a good idea to test, or 15 minutes to be more precise. Lubridate will be useful for determining time intervals
 
+# add the durations for each trip to the CleanedTrip data frame
+CleanedTrip <- mutate(CleanedTrip, TripInterval = interval(start_date,end_date))
+
+# create a list of times of 15 minute intervals over the course of a full day
+ListOfTimes <- interval(strptime("00:00", format = "%H:%M",tz="EST"),strptime("00:00", format = "%H:%M",tz="EST") +(15*60))
+InitialTime1 <- strptime("00:00", format = "%H:%M",tz="EST")
+InitialTime <- strptime("00:00", format = "%H:%M",tz="EST")
+for (i in 1:95){ #there are 96 intervals, including the initial interval that is built upon in 15 minute increments
+  InitialTime <- InitialTime +(15*60)
+  InitialInterval <- interval(InitialTime,InitialTime +(15*60))
+  ListOfTimes[i+1,]<-InitialInterval
+}
+
+# Calculate the number of days between the Trip Time interval and the base list of times
+testdif <- floor(as.numeric(difftime(InitialTime1,CleanedTrip$start_date, units="days"))) #using the floor cuts off any minutes that may errantly shift the number of days between the trip interval and teh reference interval
+# shift the Trip intervals to the date that the base list of times is set, allowing for comparison of the times
+testshift <- int_shift(CleanedTrip$TripInterval,duration(days=testdif+1))
+attr(testshift,"tzone") <- "EST" #convert the times to EST for ease of comparison with the original data
+
+CleanedTrip <- mutate(CleanedTrip, ShiftedTripInterval = testshift) #add these shifted intervals to the cleaned trip data
+
+RushHours <- data.frame(Interval = as.character(ListOfTimes[1]), NumberOfTrips = sum(int_overlaps(ListOfTimes[1],testshift))) #determine how many trips fall into the first interval
+for (i in 1:95){ #determine how many trips fall within the subsequent 95 intervals
+  Interval = as.character(ListOfTimes[i+1])
+  NumberOfTrips = sum(int_overlaps(ListOfTimes[i+1],testshift))
+  RushHours[i+1,] = list(Interval,NumberOfTrips)
+}
+
+RushHours <- cbind.data.frame(RushHours,POSIXInterval = ListOfTimes) #add the original reference list of times to the rush hours interval
+
+RushHourTimes <-RushHours[which(RushHours$NumberOfTrips>=quantile(RushHours$NumberOfTrips,probs=0.8)),]
+RushHourCutoff <- quantile(RushHours$NumberOfTrips,probs=0.8)
+
+RushHours$Interval <- str_remove_all(RushHours$Interval,"\\d{4}[-]\\d{2}[-]\\d{2}") #remove the dates from the rush hour intervals, as these will serve as the x axis values for the graph
+RushHours$Interval <- str_remove_all(RushHours$Interval,"[:]\\d{2}[ ]")
+RushHours$Interval[1] <- "00:00EST--00:15EST" #account for the fact that the first and last intervals won't be formatted properly
+RushHours$Interval[96] <- "23:45EST--00:00EST"
+
+cols <- c("white", "red")[(RushHours$NumberOfTrips > RushHourCutoff) + 1]  
+par(mar=c(6,4,4,1))
+barplot(RushHours$NumberOfTrips, col = cols, names.arg=unlist(RushHours$Interval),las=2,
+        ylab = "Number of Trips",
+        main = "Bike Usage Stratified By Time",
+        cex.names=0.5)
+
 # 6. Determine 10 most frequent starting and ending stations during rush hours
+WeekdayTrips <- CleanedTrip[-which(wday(CleanedTrip$start_date)==1 | wday(CleanedTrip$start_date)==7),]
+
+RushHourTripIndices <- int_overlaps(RushHourTimes$POSIXInterval[1],WeekdayTrips$ShiftedTripInterval)
+for (i in 1:nrow(RushHourTimes)){
+  RushHourTripIndices <- append(RushHourTripIndices,int_overlaps(RushHourTimes$POSIXInterval[i],WeekdayTrips$ShiftedTripInterval))
+} #Get an index of every trip that occurred during rush hour
+
+RushHourTrips <- WeekdayTrips[RushHourTripIndices,]
+(RushHourTrips$start_station_name)
+TopRushHourStartingStations <- count(RushHourTrips,start_station_name,sort = TRUE)
+TopRushHourEndingStations <- count(RushHourTrips,end_station_name,sort = TRUE)
 
 # 7. Determine 10 most frequent starting and ending stations during the weekends
 
+WeekendTrips <- CleanedTrip[which(wday(CleanedTrip$start_date)==1 | wday(CleanedTrip$start_date)==7),]
+WeekendTrips <- mutate(WeekendTrips,Day = wday(WeekendTrips$start_date))
+TopWeekendTripsStartingStations <- count(WeekendTrips,start_station_name,sort = TRUE)
+TopWeekendTripsEndingStations <- count(WeekendTrips,end_station_name,sort = TRUE)
+
 # 8. Determine the average utilization of bikes for each month (total time bikes were used/total time in the month)
 
+CleanedTrip <- mutate(CleanedTrip, Month = month(CleanedTrip$start_date)) #which month does the trip take place in?
+
+# calculate the length of time a bike was used according to the bike ID
+BikeIDMonthUseBase <- aggregate(TripDuration~bike_id+Month,CleanedTrip,FUN=sum)
+
+BikeIDMonthUseBase
+ggplot(BikeIDMonthUseBase, mapping = aes(x=Month, y=as.numeric(TripDuration))) + geom_point()
+
+BikeIDMonthUse <- pivot_wider(BikeIDMonthUseBase,names_from = Month, values_from = TripDuration)
+# 2014 was not a leap year, hence February has 28 days
+MonthDurations <- as.duration(days_in_month(1:12)*86400) #how many seconds are in each month?
+
+BikeIDMonthUse[,2] <- BikeIDMonthUse[,2]/MonthDurations[1]
+BikeIDMonthUse[,3] <- BikeIDMonthUse[,3]/MonthDurations[2]
+BikeIDMonthUse[,4] <- BikeIDMonthUse[,4]/MonthDurations[3]
+BikeIDMonthUse[,5] <- BikeIDMonthUse[,5]/MonthDurations[4]
+BikeIDMonthUse[,6] <- BikeIDMonthUse[,6]/MonthDurations[5]
+BikeIDMonthUse[,7] <- BikeIDMonthUse[,7]/MonthDurations[6]
+BikeIDMonthUse[,8] <- BikeIDMonthUse[,8]/MonthDurations[7]
+BikeIDMonthUse[,9] <- BikeIDMonthUse[,9]/MonthDurations[8]
+BikeIDMonthUse[,10] <- BikeIDMonthUse[,10]/MonthDurations[9]
+BikeIDMonthUse[,11] <- BikeIDMonthUse[,11]/MonthDurations[10]
+BikeIDMonthUse[,12] <- BikeIDMonthUse[,12]/MonthDurations[11]
+BikeIDMonthUse[,13] <- BikeIDMonthUse[,13]/MonthDurations[12]
+
+TopJanuaryBikes <- arrange(BikeIDMonthUse[,c(1,2)], desc(BikeIDMonthUse[,2]))
+TopFebruaryBikes <- arrange(BikeIDMonthUse[,c(1,3)], desc(BikeIDMonthUse[,3]))
+TopMarchBikes <- arrange(BikeIDMonthUse[,c(1,4)], desc(BikeIDMonthUse[,4]))
+TopAprilBikes <- arrange(BikeIDMonthUse[,c(1,5)], desc(BikeIDMonthUse[,5]))
+TopMayBikes <- arrange(BikeIDMonthUse[,c(1,6)], desc(BikeIDMonthUse[,6]))
+TopJuneBikes <- arrange(BikeIDMonthUse[,c(1,7)], desc(BikeIDMonthUse[,7]))
+TopJulyBikes <- arrange(BikeIDMonthUse[,c(1,8)], desc(BikeIDMonthUse[,8]))
+TopAugustBikes <- arrange(BikeIDMonthUse[,c(1,9)], desc(BikeIDMonthUse[,9]))
+TopSeptemberBikes <- arrange(BikeIDMonthUse[,c(1,10)], desc(BikeIDMonthUse[,10]))
+TopOctoberBikes <- arrange(BikeIDMonthUse[,c(1,11)], desc(BikeIDMonthUse[,11]))
+TopNovemebrBikes <- arrange(BikeIDMonthUse[,c(1,12)], desc(BikeIDMonthUse[,12]))
+TopDecemberBikes <- arrange(BikeIDMonthUse[,c(1,13)], desc(BikeIDMonthUse[,13]))
+
+TopBikes <- cbind(TopJanuaryBikes$bike_id,
+                  TopFebruaryBikes$bike_id,
+                  TopMarchBikes$bike_id,
+                  TopAprilBikes$bike_id,
+                  TopMayBikes$bike_id,
+                  TopJuneBikes$bike_id,
+                  TopJulyBikes$bike_id,
+                  TopAugustBikes$bike_id,
+                  TopSeptemberBikes$bike_id,
+                  TopOctoberBikes$bike_id,
+                  TopNovemebrBikes$bike_id,
+                  TopDecemberBikes$bike_id)
+
+AverageBikeUsage <- rbind(cbind(mean(as.numeric(unlist(TopJanuaryBikes[,2])), na.rm=T),
+                                mean(as.numeric(unlist(TopFebruaryBikes[,2])), na.rm=T),
+                                mean(as.numeric(unlist(TopMarchBikes[,2])), na.rm=T),
+                                mean(as.numeric(unlist(TopAprilBikes[,2])), na.rm=T),
+                                mean(as.numeric(unlist(TopMayBikes[,2])), na.rm=T),
+                                mean(as.numeric(unlist(TopJuneBikes[,2])), na.rm=T),
+                                mean(as.numeric(unlist(TopJulyBikes[,2])), na.rm=T),
+                                mean(as.numeric(unlist(TopAugustBikes[,2])), na.rm=T),
+                                mean(as.numeric(unlist(TopSeptemberBikes[,2])), na.rm=T),
+                                mean(as.numeric(unlist(TopOctoberBikes[,2])), na.rm=T),
+                                mean(as.numeric(unlist(TopNovemebrBikes[,2])), na.rm=T),
+                                mean(as.numeric(unlist(TopDecemberBikes[,2])), na.rm=T)),c(1:12))
+plot(x = AverageBikeUsage[2,], y= AverageBikeUsage[1,]*100, xlab="Month", ylab="Average Monthly Bike Utilization (%)", main="Average Bike Usage per Month")
+
+TransposeTest <- as.data.frame(t(BikeIDMonthUse[,-1]))
+colnames(TransposeTest) <- BikeIDMonthUse$bike_id
+
+# Calculate bike usage independent of ID
+
+OverallBikeUsage <- CleanedTrip[,c(3,6,15)]
+OverallBikeUsage <- mutate(OverallBikeUsage, TripInterval = iv(OverallBikeUsage$start_date,OverallBikeUsage$end_date))
+OverallBikeUsage <- OverallBikeUsage %>%
+  group_by(Month) %>%
+  reframe(TripInterval = iv_groups(TripInterval))
+
+OverallBikeUsage <- mutate(OverallBikeUsage, TripDuration = (iv_end(TripInterval)-iv_start(TripInterval)))
+OverallBikeUsage <- aggregate(TripDuration~Month,OverallBikeUsage,FUN=sum)
+
+#MonthDurations <- as.numeric(MonthDurations)
+OverallBikeUsage <- c(OverallBikeUsage[1,2]/MonthDurations[1],
+                      OverallBikeUsage[2,2]/MonthDurations[2],
+                      OverallBikeUsage[3,2]/MonthDurations[3],
+                      OverallBikeUsage[4,2]/MonthDurations[4],
+                      OverallBikeUsage[5,2]/MonthDurations[5],
+                      OverallBikeUsage[6,2]/MonthDurations[6],
+                      OverallBikeUsage[7,2]/MonthDurations[7],
+                      OverallBikeUsage[8,2]/MonthDurations[8],
+                      OverallBikeUsage[9,2]/MonthDurations[9],
+                      OverallBikeUsage[10,2]/MonthDurations[10],
+                      OverallBikeUsage[11,2]/MonthDurations[11],
+                      OverallBikeUsage[12,2]/MonthDurations[12])
+OverallBikeUsage <- cbind(OverallBikeUsage,c(1:12))
+plot(x = OverallBikeUsage[,2], y= OverallBikeUsage[,1]*100, xlab="Month", ylab="Monthly Bike Utilization (%)", main="Bike Usage per Month")
+
 # 9. Create a new dataset combining trip data with weather data. Weather data is available for each city and date. The cor function from the Corrplot package will be useful for creating a correlation matrix. Deal with the fact that it is difficult to correlate categorical and non-categorical variables. Some trips start and end in different cities, will need to explain how to correlate weather data with trip data if the trip is crossing cities - may have implications for how the two datasets are joined
+
+
+
